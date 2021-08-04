2fcbd8761151bee160053000ec3c7382c03c7c1a
LIPE CABRAL <llip2@hotmail.com>
2021-08-04 14:46:17 -0300

    First Commit

---
+++ ProAgil-App/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+import { EventoService } from './_services/evento.service';
+
+import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { DateTimeFormat } from './_helps/DateTimeFormat.pipe';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      EventosComponent,
+      NavComponent,
+      DateTimeFormat,
+   ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BsDatepickerModule.forRoot()
+  ],
+  providers: [
+    EventoService 
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+## ProAgil Eventos
+=======
+# ProAgil-frontend
+>>>>>>> acac25e04a4ed7f8d580b177abe52bd3303f765f
